d00a109f0ec18877f55d6ee8900ded2f59720d5e
Blake Renton <blake.renton.2@us.af.mil>
2023-01-06 10:00:58 -0600

    chore: Update TLDR script

--- minikube-deploy.sh
+++ minikube-deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 DEFAULT_MSSQL_PASSWORD="Passw0rd?"
 
 if [ ! -x "$(command -v minikube)" ]; then
@@ -11,64 +11,80 @@ if [ ! -x "$(command -v openssl)" ]; then
     exit 1
 fi;
 
+minikube start --dry-run 1>/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Minikube is not available. Please check your virtualization software and try again. (VirtualBox, Hyper-V, Docker, etc.)"
+    exit 1
+fi;
+
 if [ ! -x "$(command -v kubectl)" ]; then
     echo "Kubectl not found. Setting kubectl alias to minikube kubectl."
     alias kubectl='minikube kubectl --'
 fi;
 
-echo "✨ Starting minikube..."
+echo "🥳 Starting minikube..."
 minikube start
 
-echo "✨ Creating TLS certificate for DIRT..."
+echo "🔐 Creating TLS certificate for DIRT..."
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout dirt.key \
     -out dirt.crt \
     -subj "/CN=dirt.af.mil/O=dirt.af.mil" \
     -addext "subjectAltName = DNS:dirt.af.mil"
 
-echo "✨ Creating secret for DIRT TLS certificate..."
+echo "🤫 Creating secret for DIRT TLS certificate..."
 kubectl create secret tls dirt.af.mil --key dirt.key --cert dirt.crt
 
-echo "✨ Creating configmap for DIRT database schema..."
+echo "📃 Creating configmap for DIRT database schema..."
 kubectl create configmap dirt-db-config \
     --from-literal schema="$(curl -s https://raw.githubusercontent.com/elblayko/dirt-db/master/schema.sql)" \
     --from-literal accept-eula="Y"
 
-echo "✨ Deploying Ingress controller... (about a minute)"
+echo "🙌 Deploying Ingress controller... (about a minute)"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml
 kubectl rollout status deployment -n ingress-nginx ingress-nginx-controller
 
-echo "✨ Deploying DIRT... (about 2 minutes)"
-kubectl apply -f https://raw.githubusercontent.com/elblayko/dirt-kube/master/app/api.yml
-kubectl apply -f https://raw.githubusercontent.com/elblayko/dirt-kube/master/app/app.yml
-kubectl apply -f https://raw.githubusercontent.com/elblayko/dirt-kube/master/app/db.yml
-kubectl apply -f https://raw.githubusercontent.com/elblayko/dirt-kube/master/app/ingress.yml
+echo "✨ Deploying DIRT... (about 3 minutes)"
+kubectl apply \
+    -f https://raw.githubusercontent.com/elblayko/dirt-kube/master/app/api.yml \
+    -f https://raw.githubusercontent.com/elblayko/dirt-kube/master/app/app.yml \
+    -f https://raw.githubusercontent.com/elblayko/dirt-kube/master/app/db.yml \
+    -f https://raw.githubusercontent.com/elblayko/dirt-kube/master/app/ingress.yml
 kubectl rollout status deployment dirt-api
 kubectl rollout status deployment dirt-app
 kubectl rollout status statefulset dirt-db
 
-echo "✨ Sleeping for 30 seconds to allow SQL server to fully start..."
+echo "😴 Sleeping for 30 seconds to allow SQL server to fully start..."
 sleep 30
 
-echo "✨ Importing DIRT database schema..."
-kubectl exec -i statefulset/dirt-db -- bash -c "/opt/mssql-tools/bin/sqlcmd -U sa -i /var/opt/mssql/schema/schema.sql -P $DEFAULT_MSSQL_PASSWORD"
+echo "📰 Importing DIRT database schema..."
+kubectl exec -i statefulset/dirt-db -- \
+    bash -c "/opt/mssql-tools/bin/sqlcmd -U sa -i /var/opt/mssql/schema/schema.sql -P $DEFAULT_MSSQL_PASSWORD"
 
-if [ "$1" = 'dev' ]; then
-    echo "Inserting dummy data..."
+ARGS=( "$@" )
+
+if [[ "${ARGS[*]}" =~ '--with-dummy-data' ]]; then
+    echo "🌮 Inserting dummy data..."
     kubectl exec -i deployment/dirt-api -- bash -c "perl script/insert_dummy"
+fi;
+
+if [[ "${ARGS[*]}" =~ '--no-tls' ]]; then
+    echo "🔓 Disabling TLS for DIRT..."
+    kubectl patch ingress dirt --patch '{"spec": {"tls": null}}'
 fi;
 
 echo "🎉 Done!"
 
 cat << EOF
+
 Next steps:
-    1. Get the IP address of the Ingress controller, note the external IP address:
-        kubectl get svc -n ingress-nginx ingress-nginx-controller
-    2. Add the IP address to your hosts file:
+    1. Run "minikube tunnel" in a separate terminal window.  This will ask for your sudo password.
+    2. Get the IP address of the Ingress controller, note the external IP address:
+        kubectl get svc -n ingress-nginx ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
+    3. Add the IP address to your hosts file:
         echo "<EXTERNAL_IP_ADDRESS> dirt.af.mil" | sudo tee -a /etc/hosts
-    3. Run "minikube tunnel" in a separate terminal window.
     4. Open https://dirt.af.mil in your browser.
 
-    If EXTERNAL IP is not available, try running:
+    If EXTERNAL IP ADDRESS is not available, try running:
         minikube tunnel
 EOF
