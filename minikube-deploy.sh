9783e82fa7f20fd2b84e540ea186a0594b29be81
Blake Renton <blake.renton.2@us.af.mil>
2023-01-06 10:25:51 -0600

    chore: Update minikube-deploy.sh

--- minikube-deploy.sh
+++ minikube-deploy.sh
@@ -1,39 +1,51 @@
 #!/bin/bash
-DEFAULT_MSSQL_PASSWORD="Passw0rd?"
+set -e
 
+DEFAULT_MSSQL_PASSWORD="Passw0rd?"
+ARGS=("$@")
+
+# Check that minikube is installed
 if [ ! -x "$(command -v minikube)" ]; then
     echo "Minikube not found. Please install minikube and try again."
     exit 1
 fi;
 
-if [ ! -x "$(command -v openssl)" ]; then
-    echo "OpenSSL not found. Please install OpenSSL and try again."
-    exit 1
-fi;
-
+# Check that minikube driver is available
 minikube start --dry-run 1>/dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "Minikube is not available. Please check your virtualization software and try again. (VirtualBox, Hyper-V, Docker, etc.)"
     exit 1
 fi;
 
+# Check that openssl is installed
+if [ ! -x "$(command -v openssl)" ]; then
+    echo "OpenSSL not found. Please install OpenSSL and try again."
+    exit 1
+fi;
+
+# Check that kubectl is available
 if [ ! -x "$(command -v kubectl)" ]; then
     echo "Kubectl not found. Setting kubectl alias to minikube kubectl."
     alias kubectl='minikube kubectl --'
 fi;
 
-echo "🥳 Starting minikube..."
-minikube start
+# Don't start minikube, mainly for CI/CD purposes
+if [[ ! "${ARGS[*]}" =~ '--no-start-minikube' ]]; then
+    echo "🥳 Starting minikube..."
+    minikube start
+fi;
 
-echo "🔐 Creating TLS certificate for DIRT..."
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout dirt.key \
-    -out dirt.crt \
-    -subj "/CN=dirt.af.mil/O=dirt.af.mil" \
-    -addext "subjectAltName = DNS:dirt.af.mil"
+if [[ ! "${ARGS[*]}" =~ '--no-tls' ]]; then
+    echo "🔐 Creating TLS certificate for DIRT..."
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout dirt.key \
+        -out dirt.crt \
+        -subj "/CN=dirt.af.mil/O=dirt.af.mil" \
+        -addext "subjectAltName = DNS:dirt.af.mil"
 
-echo "🤫 Creating secret for DIRT TLS certificate..."
-kubectl create secret tls dirt.af.mil --key dirt.key --cert dirt.crt
+    echo "🤫 Creating secret for DIRT TLS certificate..."
+    kubectl create secret tls dirt.af.mil --key dirt.key --cert dirt.crt
+fi;
 
 echo "📃 Creating configmap for DIRT database schema..."
 kubectl create configmap dirt-db-config \
@@ -60,8 +72,6 @@ sleep 30
 echo "📰 Importing DIRT database schema..."
 kubectl exec -i statefulset/dirt-db -- \
     bash -c "/opt/mssql-tools/bin/sqlcmd -U sa -i /var/opt/mssql/schema/schema.sql -P $DEFAULT_MSSQL_PASSWORD"
-
-ARGS=( "$@" )
 
 if [[ "${ARGS[*]}" =~ '--with-dummy-data' ]]; then
     echo "🌮 Inserting dummy data..."
